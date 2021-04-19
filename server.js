35a878f6787354671ed0b36d56c8e42a8e06a2fa
Mostafa Sayed <promostafa82@gmail.com>
2021-04-19 15:42:05 +0200

    connect to database

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const colors = require("colors");
 const morgan = require("morgan");
-
-// Router files
-const categories = require("./routes/categories");
+const connectDB = require("./config/db");
 
 // Load env variables
 dotenv.config({ path: "./config/config.env" });
+
+// Connect to database
+connectDB();
+
+// Router files
+const categories = require("./routes/categories");
+const { connect } = require("./routes/categories");
 
 const app = express();
 
@@ -20,8 +26,17 @@ app.use("/api/v1/categories", categories);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode and PORT ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode and PORT ${PORT}`.yellow
+      .bold
   );
+});
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red.bold);
+
+  // Stop server & exist process
+  server.close(() => process.exit(1));
 });
