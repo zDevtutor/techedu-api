c9b3e8c557e0115caab25fd64ef9e236ee01a6bd
Mostafa Sayed <promostafa82@gmail.com>
2021-04-22 15:55:38 +0200

    setup slugify package and create category slug through mongoose hooks

--- models/Category.js
+++ models/Category.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 const CategorySchema = new mongoose.Schema({
   name: {
@@ -17,6 +18,12 @@ const CategorySchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Create category slug from name
+CategorySchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
