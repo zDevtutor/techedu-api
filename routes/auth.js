159a4e402fa265556bce6b5723dcf0a905883256
Mostafa Sayed <promostafa82@gmail.com>
2021-05-08 14:00:04 +0200

    Update user details (name, email, password)

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,9 @@ const {
   login,
   getMe,
   forgotPassword,
+  resetPassword,
+  updateDetails,
+  updatePassword,
 } = require("../controllers/auth");
 
 const router = express.Router();
@@ -13,6 +16,9 @@ const { protect } = require("../middleware/auth");
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.put("/updatedetails", protect, updateDetails);
+router.put("/updatepassword", protect, updatePassword);
 router.post("/forgotpassword", forgotPassword);
+router.put("/resetpassword", resetPassword);
 
 module.exports = router;
